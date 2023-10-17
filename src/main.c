f1832dfcd917f3beeeac27a992b0d2b70c311e1c
Luiz Felipe <61145881+lfelipediniz@users.noreply.github.com>
2023-10-16 23:40:19 -0300

    Complete the code to meet the runCodes req

--- src/main.c
+++ src/main.c
@@ -10,18 +10,33 @@ int main() {
    while (1) {
       scanf("%s", operation);
       if (strcmp(operation, "insercao") == 0) {
-         scanf("%s %s", palavra, definicao);
+         scanf("%s", palavra);
+         fgets(definicao, MAX_DEFINICAO, stdin);  // le com os espacos
+         definicao[strcspn(definicao, "\n")] =
+             '\0';  // tira a quebra de linha
          inserir_sl(dicionario, palavra, definicao);
-      } else if (strcmp(operation, "busca") == 0) {
+      }
+
+      else if (strcmp(operation, "busca") == 0) {
          scanf("%s", palavra);
          NODE* resultado = buscar_sl(dicionario, palavra);
-         printf("%s %s\n", palavra, resultado->definicao);
-      } else if (strcmp(operation, "remocao") == 0) {
+         if (resultado) {
+            printf("%s %s\n", palavra, resultado->definicao);
+         } else {
+            printf("Palavra não encontrada.\n");
+         }
+      }
+      else if (strcmp(operation, "remocao") == 0) {
          deletar_sl(dicionario, palavra);
-      } else if (strcmp(operation, "alteracao") == 0) {
-         scanf("%s %s", palavra, definicao);
+      }
+      else if (strcmp(operation, "alteracao") == 0) {
+         scanf("%s", palavra);
+         fgets(definicao, MAX_DEFINICAO, stdin); 
+         definicao[strcspn(definicao, "\n")] =
+             '\0';  
          update_sl(dicionario, palavra, definicao);
-      } else if (strcmp(operation, "impressao") == 0) {
+      }
+      else if (strcmp(operation, "impressao") == 0) {
          char ch1;
          while ((ch1 = getchar()) != '\n') {
             if (ch1 != ' ' && ch1 != '\t') {
